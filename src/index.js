8ade798bf5ca22fd6485c1b2304bfa7de61a4a0c
Jane Zhu <jane.zhu@shopify.com>
2022-12-07 13:26:59 -0600

    working counter using redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {count: 0};
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1
+      };
+    case 'decrement':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById('root'));
